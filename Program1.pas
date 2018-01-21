121cfcb5e244b19077880a30ea1a82713d585fa6
AnyaSuraykina <happyanytka25@gmail.com>
2018-01-21 23:16:48 +0300

    First two homeworks
    First two homeworks

---
+++ Program1.pas
@@ -0,0 +1,27 @@
+﻿Var
+s,s1:string;
+begin
+s:='информатика';
+writeln (s);
+writeln (copy(s,6,3));
+writeln (copy(s,8,3));
+writeln (copy(s,3,6));
+s1:=copy(s,3,5);
+writeln(s1,'-',length(s1));
+delete(s1,1,2);
+writeln(s1,'-',length(s1));
+s1:=s;delete(s1,1,5);
+delete(s1,3,2);
+insert(copy(s,5,1),s1,3);
+writeln(s1,'-',length(s1));
+s1:=copy(s,5,1)+copy(s,4,1)+copy(s,6,2)+copy(s,2,1)+copy(s,8,3);
+s1:=concat(copy(s,3,3),copy(s,8,1));
+writeln(s1,'-',length(s1));
+s1:=concat(copy(s,3,1), copy(s,1,2),copy(s,9,2));
+writeln(s1,'-',length(s1));
+s1:=concat(copy(s,10,2), copy(s,5,3));
+writeln (s1,'-',length(s1));
+s1:=copy(s,5,1)+copy(s,4,1)+copy(s,6,2)+copy(s,2,1)+copy(s,8,3)+copy(s,7,1);
+s1:=concat(copy(s,3,3),copy(s,8,1));
+writeln(s1,'-',length(s1));
+end.
---
+++ Program2dz.pas
@@ -0,0 +1,16 @@
+﻿Var
+s:string;
+begin
+s:= 'информатика';
+writeln (copy(s,6,3));
+writeln (copy(s,8,3));
+writeln (copy(s,3,6));
+writeln (copy(s,3,5));
+writeln (copy(s,5,1)+copy(s,4,1)+copy(s,6,2)+copy(s,2,1)+copy(s,8,3)+copy(s,7,1));
+writeln (copy(s,5,1)+copy(s,4,1)+copy(s,6,2)+copy(s,2,1)+copy(s,8,3));
+writeln (copy(s,5,1)+copy(s,4,1)+copy(s,6,2)+copy(s,2,1));
+writeln (copy(s,5,1)+copy(s,4,1)+copy(s,6,1));
+writeln (copy(s,6,2)+copy(s,5,1)+copy(s,8,1));
+writeln (copy(s,10,2)+copy(s,5,3));
+writeln (copy(s,3,1)+copy(s,1,2)+copy(s,9,2));
+end.
